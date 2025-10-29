1f2eedc4b1c94468c24d17c05b18a7d496010cc0
Michael <kragerles1@gmail.com>
2025-10-29 10:27:27 -0400

    Player and Zombie scripts
    Basic movement in player

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    int health = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f).normalized;
+
+        transform.position += movement * 5f * Time.deltaTime;
+    }
+
+    void healthChange(int change)
+    {
+        health += change;
+    }
+}
