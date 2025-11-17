542cd4d16a598492d91d8d69e24d91670704b2d9
Pingu-357 <orderoftheboo@gmail.com>
2025-11-17 16:25:52 -0500

    death & playing again

--- Assets/Scripts/HealthKitSpawner.cs
+++ Assets/Scripts/HealthKitSpawner.cs
@@ -6,22 +6,20 @@ public class HealthKitSpawner : MonoBehaviour
 
     float timeElapsed;
     public GameObject MedPack;
-    public GameObject Player;
 
-
-    public void RandomSpawn()
+    public void RandomHealthSpawn()
     {
         int spawnPointX = (int)UnityEngine.Random.Range(transform.position.x - 40, transform.position.x + 40);
         int spawnPointY = (int)UnityEngine.Random.Range(transform.position.y - 40, transform.position.y + 40);
-        Vector2 zombSpawnPos = new Vector2(spawnPointX, spawnPointY);
-        Instantiate(MedPack, zombSpawnPos, Quaternion.identity);
+        Vector2 HealthSpawnPos = new Vector2(spawnPointX, spawnPointY);
+        Instantiate(MedPack, HealthSpawnPos, Quaternion.identity);
     }
     void Update()
     {
         timeElapsed += Time.deltaTime;
         while ((int)timeElapsed % 5 == 0)
         {
-            RandomSpawn();
+            RandomHealthSpawn();
         }
     }
 }
