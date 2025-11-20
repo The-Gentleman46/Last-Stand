c5277a9421792de01f5fd9d3b73c76cfae204f88
The-Gentleman46 <poggies231@gmail.com>
2025-11-20 14:00:56 -0500

    Ok fixed it

--- Assets/Scripts/KillingScript.cs
+++ Assets/Scripts/KillingScript.cs
@@ -3,26 +3,18 @@ using UnityEngine;
 
 public class KillingScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-    }
-    public void OnCollisionEnter(Collider2D coll)
+    
+    public void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.CompareTag("Enemy"))
         {
-            Destroy(coll.gameObject);
+           Destroy(coll.gameObject);
            
         }
     }
-
+    /*public void AddScore(int score)
+        {
+            playerScore += score;
+        }*/
 }
 
