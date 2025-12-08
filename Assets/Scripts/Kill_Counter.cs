da72cb2040fa35b8c6b6b2ab4460effcfe82cc94
Michael <kragerles1@gmail.com>
2025-12-07 22:38:26 -0500

    Fix

--- Assets/Scripts/Kill_Counter.cs
+++ Assets/Scripts/Kill_Counter.cs
@@ -4,9 +4,12 @@ public class Kill_Counter : MonoBehaviour
 {
     private int scoreValue = 0;
     public TextMeshProUGUI score;
+    public void Update()
+    {
+        score.SetText("Zombies Killed : " + scoreValue.ToString());
+    }
     public void AddScore()
     {
         scoreValue++;
-        score.SetText("Zombies Killed : " + scoreValue.ToString());
     }
 }
